3df0ff744ebc7603b4a9d3e4ac19327a2a282aa5
vutam <tamvu.nguyen.vtp@gmail.com>
2023-03-09 21:42:21 +0700

    oop

--- module_2/src/ss4_class_oop/exercise_3/Fan.java
+++ module_2/src/ss4_class_oop/exercise_3/Fan.java
@@ -1,7 +1,73 @@
 package ss4_class_oop.exercise_3;
 
 public class Fan {
-    public static void main(String[] args) {
+    final byte Slow = 1;
+    final byte medium = 2;
+    final byte fast = 3;
+
+    int speed = Slow;
+    boolean on = false;
+    double radius = 5;
+    String color = "blue";
+
+    public byte getSlow() {
+        return Slow;
+    }
+
+    public byte getMedium() {
+        return medium;
+    }
+
+    public byte getFast() {
+        return fast;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        if (getOn()) {
+            this.speed = 0;
+        } else {
+            this.speed = speed;
+        }
+    }
+
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public boolean getOn() {
+        return this.on;
+    }
+
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String toString() {
+        return "Fan{" +
+                ", speed=" + speed +
+                ", on=" + on +
+                ", radius=" + radius +
+                ", color='" + color + '\'' +
+                '}';
 
     }
 }
+
--- module_2/src/ss4_class_oop/exercise_3/FanMain.java
+++ module_2/src/ss4_class_oop/exercise_3/FanMain.java
@@ -1,7 +1,22 @@
 package ss4_class_oop.exercise_3;
-
+import java.util.Scanner;
 public class FanMain {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Fan fan1 = new Fan();
+        fan1.setOn(true);
+        fan1.setRadius(10);
+        fan1.setColor("Yellow");
+        fan1.setSpeed(fan1.Slow);
+        System.out.println(fan1.toString());
+
+        Fan fan2 = new Fan();
+        fan2.setOn(false);
+        fan2.setRadius(5);
+        fan2.setColor("blue");
+        fan2.setSpeed(fan2.medium);
+        System.out.println(fan2.toString());
+
 
     }
 }

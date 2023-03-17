90397f3ef2119459870990bf9414af3fc12401ea
vutam <tamvu.nguyen.vtp@gmail.com>
2023-03-17 10:11:55 +0700

    ss10

---
+++ module_2/src/ss10_array_linked/exercise_1_1/MyList.java
@@ -0,0 +1,68 @@
+package ss10_array_linked.exercise_1_1;
+
+import java.util.Arrays;
+
+public class MyList<E> {
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+    Object elements[];
+ 
+    public MyList() {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyList(int capacity) {
+        if (capacity >= 0) {
+            elements = new Object[capacity];
+        } else {
+            throw new IllegalArgumentException("capacity: " + capacity);
+        }
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public void clear() {
+        size = 0;
+        for (int i = 0; i < elements.length; i++) {
+            elements[i] = null;
+        }
+    }
+
+    boolean add(E element) {
+        if (elements.length == size) {
+            this.ensureCapacity(5);
+        }
+        elements[size] = element;
+        size++;
+        return true;
+    }
+
+    public void add(E element, int index) {
+        if (index > elements.length) {
+            throw new IllegalArgumentException("index: " + index);
+        } else if (elements.length == size) {
+            this.ensureCapacity(5);
+        }
+        if (elements[index] == null) {
+            elements[index] = element;
+            size++;
+        } else {
+            for (int i = 0; i < index; i++) {
+                elements[i] = elements[i - 1];
+            }
+            elements[index] = element;
+            size++;
+        }
+    }
+
+    private void ensureCapacity(int minCapacity) {
+        if (minCapacity == 0) {
+            int newSize = this.elements.length + minCapacity;
+            elements = Arrays.copyOf(elements, newSize);
+        } else {
+            throw new IllegalArgumentException("minCapacity: " + minCapacity);
+        }
+    }
+}

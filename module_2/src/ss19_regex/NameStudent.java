75c2503ef251ac7abe5c7d4b3d822a88f3359be2
vutam <tamvu.nguyen.vtp@gmail.com>
2023-03-31 16:23:07 +0700

    fix

--- module_2/src/ss19_regex/NameStudent.java
+++ module_2/src/ss19_regex/NameStudent.java
@@ -1,2 +1,19 @@
-package ss19_regex;public class NameStudent {
+package ss19_regex;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class NameStudent {
+    private static final String REGEX_CLASS = "^([A-Z][a-z]{1,7}\\s){1,5}([A-Z][a-z]{1,7})";
+
+    public static boolean namevalidateStudent(String regex) {
+        return Pattern.matches(REGEX_CLASS, regex);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhập tên học sinh");
+        String studentName = sc.nextLine();
+        System.out.println(namevalidateStudent(studentName));
+    }
 }
--- module_2/src/ss19_regex/ValidateNameForClassName.java
+++ module_2/src/ss19_regex/ValidateNameForClassName.java
@@ -12,7 +12,7 @@ public class ValidateNameForClassName {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("nhap ten lop");
+        System.out.println("Nhập tên lớp");
         String classesName = sc.nextLine();
         System.out.println(validateClassName(classesName));
     }
